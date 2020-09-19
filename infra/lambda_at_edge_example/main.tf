b4139ab26ec62d144d5aafff286ea01a633b199e
dmattia <david@transcend.io>
2020-09-19 10:50:11 -0500

    Make Lambda@Edge use typescript

--- infra/lambda_at_edge_example/main.tf
+++ infra/lambda_at_edge_example/main.tf
@@ -3,16 +3,6 @@ provider "aws" {
   region  = "us-east-1"
 }
 
-/** Create the subdomain edge.acme-example.com */
-module "domain" {
-  source           = "git::https://github.com/cloudposse/terraform-aws-route53-cluster-zone.git?ref=master"
-  namespace        = "blog"
-  stage            = "dev"
-  name             = "edge"
-  parent_zone_name = "acme-example.com"
-  zone_name        = "$${name}.$${parent_zone_name}"
-}
-
 /** Create a Lambda@Edge function */
 module "security_header_lambda" {
   source                 = "transcend-io/lambda-at-edge/aws"
@@ -20,7 +10,7 @@ module "security_header_lambda" {
   name                   = "security_headers"
   description            = "Adds security headers to the response"
   runtime                = "nodejs12.x"
-  lambda_code_source_dir = "${path.module}/../../src/security_headers"
+  lambda_code_source_dir = "${path.module}/../../src/security_headers/dist"
 }
 
 /** Create the S3 bucket with CloudFront distribution necessary to host the site */
@@ -30,12 +20,6 @@ module "cloudfront-s3-cdn" {
 
   name               = "edge-acme-example"
   encryption_enabled = true
-
-  # DNS Settings
-  parent_zone_id      = module.domain.zone_id
-  acm_certificate_arn = module.acm_request_certificate.arn
-  aliases             = [module.domain.zone_name]
-  ipv6_enabled        = true
 
   # Caching Settings
   default_ttl = 300
@@ -47,25 +31,11 @@ module "cloudfront-s3-cdn" {
   error_document  = "index.html"
 
   # Lambda@Edge setup
-  lambda_function_association = [
-    {
-      event_type   = "origin-response"
-      include_body = false
-      lambda_arn   = module.security_header_lambda.arn
-    },
-  ]
-
-  depends_on = [module.acm_request_certificate, module.domain]
-}
-
-/** Request an SSL certificate */
-module "acm_request_certificate" {
-  source                      = "cloudposse/acm-request-certificate/aws"
-  version                     = "0.7.0"
-  domain_name                 = module.domain.zone_name
-  wait_for_certificate_issued = true
-
-  depends_on = [module.domain]
+  lambda_function_association = [{
+    event_type   = "origin-response"
+    include_body = false
+    lambda_arn   = module.security_header_lambda.arn
+  }]
 }
 
 /** Use remote state through terraform cloud */
